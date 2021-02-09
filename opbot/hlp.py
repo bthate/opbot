2531d50ae13aa4baaf40d4462b3c1272db6e40e2
Bart Thate <bart@dds.nl>
2021-02-09 14:31:00 +0100

    add help txt

---
+++ opbot/hlp.py
@@ -0,0 +1,36 @@
+# This file is placed in the Public Domain
+
+"""OPBOT(1)			   User Commands 			 OPBOT(1)
+
+NAME
+        OPBOT - operbot.
+
+SYNOPSIS
+        opbot <cmd> [mods=mod1,mod2] [-d] [-h] [-s] [-v]
+
+DESCRIPTION
+        OPBOT is a pure python3 IRC chat bot that can run as a background daemon
+        for 24/7 a day presence in a IRC channel. It installs itself as a service so
+        you can get it restarted on reboot. You can use it to display RSS feeds, act as a
+        UDP to IRC gateway, program your own commands for it, have it log objects on
+        disk and search. 
+
+        OPBOT is placed in the Public Domain, no COPYRIGHT, no LICENSE.
+
+EXAMPLES
+        1) opbot cfg server=<server> channel=<channel> nick=<nick>
+        2) opbot met <userhost>
+        3) opbot rss <url>
+        4) opbot ftc
+        5) opbot cmd
+        6) opbot mods=irc
+
+OPTIONS
+        -c		start console
+        -l		read config
+        -r		use /var/lib/genocide/
+        -s              save config
+        -h              print this message
+        -v              be verbose
+        -z		use ~/.genocide
+"""
